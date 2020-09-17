4b295acecb97a9e16e800ab4b0f0ae4bbf7b1d5a
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-09-17 17:03:32 -0400

    Add some changes in styling

--- challenges/src/Component/homepage.jsx
+++ challenges/src/Component/homepage.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-import './homepage.css'
+import "./homepage.css";
 
 function homepage() {
   return (
-      <div className="Home">
+    <div className="Home">
+      <div className="main_work">
+        <h1 className="main_heading">React</h1>
+        <p>A JavaScript library for building user interfaces</p>
 
-          <div className="main_work">
-              <h1 className="main_heading">React</h1>
-              <p>A JavaScript library for building user interfaces</p>
-              
-              <div className="buttons">
-                  <div className="getstarted">
-                     Get Started
-                  </div>
-                  <div className="taking_tutorial">
-                      Take the tutorial
-                  </div>
-              </div>
-          </div>
-       
+        <div className="buttons">
+          <div className="getstarted">Get Started</div>
+          <div className="taking_tutorial">Take the tutorial </div>
+        </div>
       </div>
-
+    </div>
   );
 }
 
