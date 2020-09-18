b0874512418a6c443fda424ef074a6b6a0cfb1df
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-09-18 04:31:48 -0400

    complete header & homepage

--- challenges/src/Component/header.jsx
+++ challenges/src/Component/header.jsx
@@ -16,6 +16,13 @@ const Header = () => {
       </div>
 
       <div className="header__nav">
+        <img
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+          alt=""
+          height="20"
+        />
+        <span className="react">REACT</span>
+
         <div className="header__option">Docs</div>
 
         <div className="header__option">Tutorial</div>
@@ -26,6 +33,17 @@ const Header = () => {
         <Link to="/login">
           <div className="header__option">Login</div>
         </Link>
+        <input type="text" placeholder="Search" />
+
+        <div style={{ fontSize: "15px" }} className="header__option">
+          v16.13.1
+        </div>
+        <div style={{ fontSize: "15px" }} className="header__option">
+          Languages
+        </div>
+        <div style={{ fontSize: "15px" }} className="header__option">
+          Github
+        </div>
       </div>
     </div>
   );
--- challenges/src/Component/homepage.jsx
+++ challenges/src/Component/homepage.jsx
@@ -10,7 +10,24 @@ function homepage() {
 
         <div className="buttons">
           <div className="getstarted">Get Started</div>
-          <div className="taking_tutorial">Take the tutorial </div>
+          <div className="taking_tutorial">
+            Take the tutorial
+            <svg
+              height="12"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 4.53657 8.69699"
+              style={{ marginLeft: "10px" }}
+            >
+              <path
+                d="
+        M.18254,8.697a.18149.18149,0,0,1-.12886-.31034L4.09723,4.34126.05369.29954a.18149.18149,
+        0,0,1,.2559-.2559L4.4838,4.21785a.18149.18149,0,0,1,0,.2559L.30958,8.648A.18149.18149,
+        0,0,1,.18254,8.697Z
+      "
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
